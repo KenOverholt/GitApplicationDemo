e444cf576da200bc6307af887ee2dbf04e16191d
Ken <kroverholt@gmail.com>
2020-04-27 12:02:19 -0400

    Added button; resized text

--- app/src/main/java/com/kroverholt/gitapplicationdemo/MainActivity.java
+++ app/src/main/java/com/kroverholt/gitapplicationdemo/MainActivity.java
@@ -3,12 +3,33 @@ package com.kroverholt.gitapplicationdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView mainMessage = findViewById(R.id.tvMainMessage);
+        Button myButton = findViewById(R.id.btnToggle);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mainMessage.getVisibility() == View.INVISIBLE)
+                    mainMessage.setVisibility(View.VISIBLE);
+                else if (mainMessage.getVisibility() == View.VISIBLE)
+                    mainMessage.setVisibility(View.INVISIBLE);
+                else
+                    Log.i(TAG, "onClick: Unable to set visibility");
+            }
+        });
     }
+
 }
